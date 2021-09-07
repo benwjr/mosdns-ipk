812600d182e65a2ab5757f509d4dc320f9c012ee
Sempr <iamsempr@gmail.com>
2021-09-07 17:48:54 +0800

    MOD: rename ipk

--- scripts/download.sh
+++ scripts/download.sh
@@ -39,8 +39,8 @@ pushd data
 tar cvzf ../data.tar.gz ./*
 popd
 echo 2.0 >debian-binary
-tar cvzf mosdns-go-1.0.ipk control.tar.gz data.tar.gz debian-binary
-export PACKAGE_OUTPUT_PATH=${PWD}/mosdns-go-1.0.ipk
+tar cvzf mosdns-go_1.0.ipk control.tar.gz data.tar.gz debian-binary
+export PACKAGE_OUTPUT_PATH=${PWD}/mosdns-go_1.0.ipk
 popd
 
 popd
